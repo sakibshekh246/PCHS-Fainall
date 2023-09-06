2de0d305029445a6e5be2ce5be1828f276496a8e
Md Sakib Shekh <140169239+sakibshekh246@users.noreply.github.com>
2023-09-06 16:56:24 +0600

    change

--- pchs/views.py
+++ pchs/views.py
@@ -3,5 +3,9 @@ def index(request):
     return render(request,'pchs.html')
 def insert(request):
     cat_name = request.POST.get('user_name')
-    Email = request.POST.get('email')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    password = request.POST.get('com_password')
+    number = request.POST.get('number')
+    
     return HttpResponse(cat_name)
