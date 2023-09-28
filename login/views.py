b4cbc4022b9fe2b2783f902e3d374c4d264d9f4b
Md Sakib Shekh <140169239+sakibshekh246@users.noreply.github.com>
2023-09-29 03:14:11 +0600

    ?

--- login/views.py
+++ login/views.py
@@ -8,6 +8,25 @@ def index(request):
     r_data = {'data' : Registration.objects.all()}
     return render(request, './login.html',r_data)
 
+ def login(request):
     
+            user = authenticate(username=username, password=password)
+            if user:
+                login(request, user)
+                if not remember_me:
+                    request.session.set_expiry(0)
+                    return redirect('accounts:home')
+                else:
+                    request.session.set_expiry(1209600)
+                    return redirect('accounts:home')
+            else:
+                return redirect('accounts:login')
+        else:
+            return redirect('accounts:registration')
+    else:
+        form = LoginForm()
+        return render(request, "loginadmin")   
+
+
 
 # Create your views here.
--- pchs/settings.py
+++ pchs/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'pchs',
     'Registration',
-    'ckeditor'
+    'ckeditor',
+    'Login'
 ]
 
 MIDDLEWARE = [
