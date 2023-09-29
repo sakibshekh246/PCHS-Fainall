05acb64d6239cb11f53271f6bc769365f5db0a8d
Md Sakib Shekh <140169239+sakibshekh246@users.noreply.github.com>
2023-09-29 20:27:58 +0600

    error

--- pchs/models.py
+++ pchs/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from Registration.models import Registration
+from login.models import Login
 class User_Name(models.Model):
     id = models.AutoField(primary_key=True)
     user_name = models.CharField(max_length=200, default="0", unique=True)
